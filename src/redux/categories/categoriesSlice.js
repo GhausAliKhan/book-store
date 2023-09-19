90bf05ff6cd1c07041aa44ca0d4f419a149f9e5d
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-09-19 18:19:28 +0500

    created categoriesSlice file

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState: {
+    categories: [],
+  },
+  reducers: {
+    checkStatus: (state) => {
+      state.categories = 'Under construction';
+    },
+  },
+});
+
+export const { checkStatus } = categoriesSlice.actions;
+
+export default categoriesSlice.reducer;
